047ffbbd2afbdd0aae72ef51d99159f551f605b7
Argha Sarkar <argha.sarkar1994@gmail.com>
2017-02-25 18:31:09 +0000

    WIP: added express

--- server/index.js
+++ server/index.js
@@ -1,2 +1,16 @@
-var express = require("express");
+const PORT = 8080;
+const HOST = "0.0.0.0";
 
+var express = require("express");
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+var server = app.listen(PORT, HOST, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Example app listening at http://%s:%s", host, port)
+});
